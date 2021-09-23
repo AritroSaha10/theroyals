e57ef03ad5cbec2f6649b7d4dc73c9100842ca79
Aritro Saha <29025984+AritroSaha10@users.noreply.github.com>
2021-09-23 19:07:20 -0400

    feat: finished about page

--- pages/about.js
+++ pages/about.js
@@ -4,6 +4,7 @@ import Link from 'next/link'
 import Layout from "../components/Layout"
 
 import Interested from "../public/images/teamspraypaint.jpg"
+import TippingPointField from "../public/images/tipping-point-field.png"
 
 export default function AboutUs() {
     return (
@@ -25,34 +26,61 @@ export default function AboutUs() {
                     </div>
                 </div>
             </header>
-        
-            <div className="text-center lg:text-left px-10 py-2 lg:px-20 lg:py-4 xl:px-60 xl:py-10">
+
+            <div className="text-center lg:text-left px-10 pt-6 pb-2 lg:px-20 xl:px-60 lg:pt-12 lg:pb-4">
                 <h1 className="text-3xl font-semibold mb-2">What is VEX?</h1>
 
                 <p className="text-md text-gray-600">
-                    The VEX Robotics Competition is a popular robotics program 
-                    for students worldwide, held by Innovation First International. 
-                    Teams build a robot with provided VEX parts and program it to run 
-                    through a course and complete objectives, such as throwing a ball 
-                    in a hoop or stacking cubes into towers and cups. Each challenge allows 
+                    The VEX Robotics Competition is a popular robotics program
+                    for students worldwide, held by Innovation First International.
+                    Teams build a robot with provided VEX parts and program it to run
+                    through a course and complete objectives, such as throwing a ball
+                    in a hoop or stacking cubes into towers and cups. Each challenge allows
                     students to expand their knowledge within the domains of engineering, design,
-                    programming, and of course, teamwork! For VRC specifically, high school and 
-                    middle school students are able to compete with their built robots. For younger 
+                    programming, and of course, teamwork! For VRC specifically, high school and
+                    middle school students are able to compete with their built robots. For younger
                     students, VEX-IQ is available, and for post-secondary students, VEXU is available.
                 </p>
+
+                <a
+                    className="text-blue-500 hover:text-blue-700 duration-150"
+                    href="https://www.vexrobotics.com/"
+                    target="_blank"
+                    rel="noreferrer"
+                >
+                    Learn more at the VEX robotics website.
+                </a>
             </div>
 
-            <div className="text-center lg:text-left px-10 py-2 lg:px-20 xl:px-60">
+            <div className="text-center lg:text-left px-10 py-4 lg:px-20 xl:px-60">
                 <h1 className="text-3xl font-semibold mb-2">Why should you join?</h1>
 
                 <p className="text-md text-gray-600">
-                VEX is an excellent learning opportunity for students - just ask our team members! 
-                    Building a robot from the ground up with your team teaches you to think critically 
-                    and learn much more about design. Students get to become part of a team working towards 
-                    the same objective, building tight-knit bonds and friendships with their teammates. 
-                    Participating in competitions with your team is also an excellent experience, allowing 
+                    VEX is an excellent learning opportunity for students - just ask our team members!
+                    Building a robot from the ground up with your team teaches you to think critically
+                    and learn much more about design. Students get to become part of a team working towards
+                    the same objective, building tight-knit bonds and friendships with their teammates.
+                    Participating in competitions with your team is also an excellent experience, allowing
                     members to meet other individuals who are passionate for STEM.
                 </p>
+            </div>
+
+            <div className="text-center lg:text-left px-10 py-4 lg:px-20 xl:px-60">
+                <h1 className="text-3xl font-semibold mb-2">2021-2022 Season: Tipping Point</h1>
+
+                <p className="text-md text-gray-600 mb-2">
+                    For this year's season, the object of the game is to attain a higher score than the opposing Alliance by Scoring Rings,
+                    moving Mobile Goals to Alliance Zones, and by Elevating on Platforms at the end of a Match. 
+                </p>
+
+                <p className="text-md text-gray-600 mb-2">
+                    The game is played on a 12’x12’ square field configured as seen.
+                    Two Alliances, one “red” and one “blue”, composed of two teams each, compete in matches
+                    consisting of a fifteen second Autonomous Period (where the robot must score points by itself), 
+                    followed by a 1m45s Driver Controlled Period.
+                </p>
+
+                <Image src={TippingPointField} placeholder="blur" quality={100} />
             </div>
         </Layout>
     )
