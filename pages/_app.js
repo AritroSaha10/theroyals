5136de563d5d70206f92117574b879d27b851bcd
Aritro <29025984+AritroSaha10@users.noreply.github.com>
2021-09-30 23:40:36 -0400

    feat: added firebase analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,35 @@
 import 'tailwindcss/tailwind.css'
 import { AnimateSharedLayout } from "framer-motion"
 
+import { useEffect } from 'react'
+import { useRouter } from 'next/router'
+
+import analytics from "../util/firebase/analytics"
+import { logEvent, setCurrentScreen } from "firebase/analytics"
+
 function MyApp({ Component, pageProps }) {
+  const router = useRouter();
+
+  useEffect(() => {
+    if (process.env.NODE_ENV === "production") {
+      // Initialize Firebase Analytics
+      const logEventHandler = (url) => {
+        setCurrentScreen(analytics(), url);
+        logEvent(analytics(), "screen_view");
+      };
+
+      router.events.on('routeChangeComplete', logEventHandler);
+      logEventHandler(window.location.pathname); // Logs first page that the user visits
+
+      console.log("Made with no ☕️!");
+
+      // Remvove the event listener for firebase Analytics after unmount
+      return () => {
+        router.events.off('routeChangeComplete', logEventHandler);
+      };
+    }
+  }, [router.events]);
+
   return (
     <AnimateSharedLayout>
       <Component {...pageProps} />
---
+++ util/firebase/app.js
@@ -0,0 +1,28 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp, getApps } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyDUiF-azutL9yL_SJtIy-3FZMci5NrUCqU",
+    authDomain: "theroyals-vex.firebaseapp.com",
+    projectId: "theroyals-vex",
+    storageBucket: "theroyals-vex.appspot.com",
+    messagingSenderId: "342959534793",
+    appId: "1:342959534793:web:255b451e66823c8cfed318",
+    measurementId: "G-NB46Y2GW0F"
+};
+
+let app;
+
+if (getApps().length === 0) {
+    // Initialize Firebase
+    app = initializeApp(firebaseConfig);
+} else {
+    app = getApps()[0];
+}
+
+export default app;
